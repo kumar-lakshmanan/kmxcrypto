d180e09fb2c45cb3305703e95af678e4964c6ed9
kaymatrix <kaymatrix@gmail.com>
2025-06-11 16:51:38 +0530

    part 2

--- lib/organizer.py
+++ lib/organizer.py
@@ -28,8 +28,10 @@ class WinnerChecker():
 class FetchCoin():
     def __init__(self):
         self.tls = kTools.KTools()
-        self.lfs = local_firestore.LocalFireStore()
-        self.gfs = gcp_firestore.GCPFireStore()
+        if self.tls.isProd() and not self.tls.isLocal():
+            self.gfs = gcp_firestore.GCPFireStore()
+        else:
+            self.lfs = local_firestore.LocalFireStore()
 
     def getTodaysCollection(self):
         if self.tls.isProd() and not self.tls.isLocal():
